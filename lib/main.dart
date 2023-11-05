91ff93772b1a6c5e3b197d770f479540d8629181
Yehor Pustovoit <63584ee@gmail.com>
2023-11-05 17:17:10 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:local_connect/features/auth/presentation/pages/auth_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
+      ),
+      home: const AuthPage(),
+    );
+  }
+}
